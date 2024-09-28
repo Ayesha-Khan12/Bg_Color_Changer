b45320b2e5667f75df1c6e7bd8345d2a7e126980
Ayesha-Khan12 <developerayesha582@gmail.com>
2024-09-28 18:16:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function color() {
+    document.body.style.background = `linear-gradient(to right, rgb(${Math.floor(Math.random() * 255)},
+     ${Math.floor(Math.random() * 255)},
+      ${Math.floor(Math.random() * 255)}),
+    rgb(${Math.floor(Math.random() * 255)},
+     ${Math.floor(Math.random() * 255)},
+      ${Math.floor(Math.random() * 255)}))`
+}
